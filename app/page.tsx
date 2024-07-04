808b9646fce8ff6ecb338ecb3d788e8c2775a0ea
jvras <jonathas.vincius@ufpe.br>
2024-07-04 15:22:57 -0300

    feat: HomePage / layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,40 @@
-export default function Home() {
-  return <main>Home</main>;
+import Photo from "@/components/Photo";
+
+import { FaWpforms } from "react-icons/fa6";
+
+const Home = () => {
+  return (
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+      <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+      {/* texto */}
+      <div className="text-center xl:text-left order-2 xl:order-none">
+        <span className="text-xl">IDW 24.1</span>
+        <h1 className="h1 mb-6">
+          I Webinário<br/> <span className="text-accent">Sobre Dados</span>
+        </h1>
+        <p className="max-w-[500px] mb-9 text-white/80"> 
+        Um webinário é uma webconferência ou videoconferência com intuito educacional, 
+        no qual a comunicação é de apenas uma via, ou seja, somente o palestrante se expressa e as outras assistem, 
+        onde a interação dos ...
+        </p>
+        {/* botão e redes Rede */}
+        <div className="flex flex-col xl:flex-row items-center gap-8">
+          <a className="uppercase flex items-center gap-2 border border-accent rounded-full px-4 py-2 hover:bg-accent hover:text-primary transition-all duration-500">
+          <span>Inscreva-se</span>
+          <FaWpforms className="text-xl" />
+          </a>
+          <div className="mb-8 xl:mb-0">
+          </div>
+        </div>
+      </div>
+      {/* foto */}
+      <div className="order-1 xl:order-none mb-8 xl:mb-0">
+        <Photo />
+      </div>
+      </div>
+      </div>
+    </section>
+  );
 }
+export default Home;
---
+++ components/WorkSliderBtns.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useSwiper } from "swiper/react";
+import { PiCaretLeftBold, PiCaretRightBold } from "react-icons/pi";
+
+interface WorkSliderBtnsProps {
+    containerStyles: string;
+    btnStyles: string;
+    iconsStyle: string;
+}
+
+const WorkSliderBtns = ({containerStyles, btnStyles, iconsStyle}: WorkSliderBtnsProps) => {
+    const swiper = useSwiper();
+    return (
+        <div className={containerStyles}>
+        <button className={btnStyles} onClick={() => swiper.slidePrev()}>
+            <PiCaretLeftBold className={iconsStyle} />
+        </button>
+        <button className={btnStyles} onClick={() => swiper.slideNext()}>
+            <PiCaretRightBold className={iconsStyle} />
+        </button>
+        </div>
+    );
+};
+export default WorkSliderBtns;
